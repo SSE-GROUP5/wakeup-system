8ead865acbde0bf8c799f932952d967706e98a18
David Frischer <david.frischer@frischer.ch>
2024-01-25 23:31:48 +0000

    [wakeup] Add support for fake HA for dev purposes

---
+++ wakeup-server/homeassistant/fake_matter_device.py
@@ -0,0 +1,16 @@
+
+FAKE_MATTER_DEVICE = {
+    "matter_id": "fake-matter-device",
+    "name": "Fake Matter Device",
+    "type": "switch",
+    "possible_actions": [{
+        "name": "Turn On",
+        "action": "turn_on"
+      }, {
+        "name": "Turn Off",
+        "action": "turn_off"
+      }, {
+        "name": "Toggle",
+        "action": "toggle"
+      }]
+}
