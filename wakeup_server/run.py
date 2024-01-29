de2b23e2746ecae7ec8210d8729041920f8e6deb
Kang2001 <94216961+Kang2001@users.noreply.github.com>
2024-01-29 15:42:25 +0000

    [swagger] Add swagger doc for wakeup server
    Initial implementation of swagger documentation.
    Complete documentation of all wakeup module back-end interfaces.

--- wakeup_server/run.py
+++ wakeup_server/run.py
@@ -1,22 +1,40 @@
 from flask import Flask
+from flasgger import Swagger
+
+from flask import Flask
 from constants import PORT, HOSTNAME
 from blueprints.triggers.triggers import triggers_blueprint
 from blueprints.signals.signals import signals_blueprint
 from blueprints.target_devices.target_devices import target_devices_blueprint
 from blueprints.users.users import users_blueprint
+from blueprints.docs.docs import get_swagger_config
 from homeassistant_client import homeassistant_client
 from db import db
 from log_scheduler import LogScheduler
+from flask_swagger_ui import get_swaggerui_blueprint
+import os
+
+SWAGGER_URL = '/docs'
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
 
 def create_app(url="sqlite:///wakeup.sqlite"):
-    app = Flask(__name__)
+    app = Flask(__name__, static_url_path='/static')
     app.config['DEBUG'] = True
     app.config['SQLALCHEMY_DATABASE_URI'] = url
-    
+
     app.register_blueprint(triggers_blueprint)
     app.register_blueprint(signals_blueprint)
     app.register_blueprint(target_devices_blueprint)
     app.register_blueprint(users_blueprint)
+    config = get_swagger_config()
+    swaggerui_blueprint = get_swaggerui_blueprint(
+        SWAGGER_URL,
+        config['API_URL'],
+        config=config
+    )
+    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+    
     @app.route('/health')
     def health_check():
         is_HA_running = homeassistant_client.health_check()
@@ -45,6 +63,7 @@ def create_app(url="sqlite:///wakeup.sqlite"):
 
 
 if __name__ == "__main__":
-    
     app = create_app()
+    print(f"Swagger UI available at http://{HOSTNAME}:{PORT}/docs")
     app.run(port=PORT, host=HOSTNAME)
+
