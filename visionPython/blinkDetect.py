0ff4da57a0388e0d56689897738891f2b0df744d
JIngyuan <Andydiantu233@outlook.com>
2024-02-04 23:02:49 +0000

    added example file for tkinter gui
    added read env file function
    
    added file check
    
    changed constants to .venv from json
    
    [sound] Add tkinter in the audio classification
    
    changed constant to reading env file
    
    Integrate TKinter with blinkDetect
    
    [Vision tkinter]added runtime change for constants
    
    undo changes of tkinter
    
    delete testing files
    
    delete test chanegs

--- visionPython/blinkDetect.py
+++ visionPython/blinkDetect.py
@@ -32,39 +32,7 @@ def send_signal(action: str):
     print(response)
 
 
-# variables 
-frame_counter =0
-CEF_COUNTER =0
-TOTAL_BLINKS =0
 
-config = Constants()
-
-
-FONTS =cv.FONT_HERSHEY_COMPLEX
-LAST_BLINKING_TIME = time.time()
-client = Session()
-client.headers.update({'Content-Type': 'application/json'})
-client.headers.update({'Accept': 'application/json'})
-mqServer = ZeroMQServer("tcp://*:5556")
-
-# face bounder indices 
-FACE_OVAL=[ 10, 338, 297, 332, 284, 251, 389, 356, 454, 323, 361, 288, 397, 365, 379, 378, 400, 377, 152, 148, 176, 149, 150, 136, 172, 58, 132, 93, 234, 127, 162, 21, 54, 103,67, 109]
-
-# lips indices for Landmarks
-LIPS=[ 61, 146, 91, 181, 84, 17, 314, 405, 321, 375,291, 308, 324, 318, 402, 317, 14, 87, 178, 88, 95,185, 40, 39, 37,0 ,267 ,269 ,270 ,409, 415, 310, 311, 312, 13, 82, 81, 42, 183, 78 ]
-LOWER_LIPS =[61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291, 308, 324, 318, 402, 317, 14, 87, 178, 88, 95]
-UPPER_LIPS=[ 185, 40, 39, 37,0 ,267 ,269 ,270 ,409, 415, 310, 311, 312, 13, 82, 81, 42, 183, 78] 
-# Left eyes indices 
-LEFT_EYE =[ 362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385,384, 398 ]
-LEFT_EYEBROW =[ 336, 296, 334, 293, 300, 276, 283, 282, 295, 285 ]
-
-# right eyes indices
-RIGHT_EYE=[ 33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161 , 246 ]  
-RIGHT_EYEBROW=[ 70, 63, 105, 66, 107, 55, 65, 52, 53, 46 ]
-
-map_face_mesh = mp.solutions.face_mesh
-# camera object 
-camera = cv.VideoCapture(0)
 # landmark detection function 
 def landmarksDetection(img, results, draw=False):
     img_height, img_width= img.shape[:2]
@@ -117,6 +85,39 @@ def blinkRatio(img, landmarks, right_indices, left_indices):
     ratio = (reRatio+leRatio)/2
     return ratio 
 
+# variables 
+frame_counter =0
+CEF_COUNTER =0
+TOTAL_BLINKS =0
+
+config = Constants()
+
+
+FONTS =cv.FONT_HERSHEY_COMPLEX
+LAST_BLINKING_TIME = time.time()
+client = Session()
+client.headers.update({'Content-Type': 'application/json'})
+client.headers.update({'Accept': 'application/json'})
+mqServer = ZeroMQServer("tcp://*:5556")
+
+# face bounder indices 
+FACE_OVAL=[ 10, 338, 297, 332, 284, 251, 389, 356, 454, 323, 361, 288, 397, 365, 379, 378, 400, 377, 152, 148, 176, 149, 150, 136, 172, 58, 132, 93, 234, 127, 162, 21, 54, 103,67, 109]
+
+# lips indices for Landmarks
+LIPS=[ 61, 146, 91, 181, 84, 17, 314, 405, 321, 375,291, 308, 324, 318, 402, 317, 14, 87, 178, 88, 95,185, 40, 39, 37,0 ,267 ,269 ,270 ,409, 415, 310, 311, 312, 13, 82, 81, 42, 183, 78 ]
+LOWER_LIPS =[61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291, 308, 324, 318, 402, 317, 14, 87, 178, 88, 95]
+UPPER_LIPS=[ 185, 40, 39, 37,0 ,267 ,269 ,270 ,409, 415, 310, 311, 312, 13, 82, 81, 42, 183, 78] 
+# Left eyes indices 
+LEFT_EYE =[ 362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385,384, 398 ]
+LEFT_EYEBROW =[ 336, 296, 334, 293, 300, 276, 283, 282, 295, 285 ]
+
+# right eyes indices
+RIGHT_EYE=[ 33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161 , 246 ]  
+RIGHT_EYEBROW=[ 70, 63, 105, 66, 107, 55, 65, 52, 53, 46 ]
+
+map_face_mesh = mp.solutions.face_mesh
+# camera object 
+camera = cv.VideoCapture(0)
 
 
 with map_face_mesh.FaceMesh(min_detection_confidence =0.5, min_tracking_confidence=0.5) as face_mesh:
@@ -134,6 +135,7 @@ with map_face_mesh.FaceMesh(min_detection_confidence =0.5, min_tracking_confiden
         message = mqServer.receive()
         if message != None:
             topic, msg = message
+
             if topic == "Camera":
                 config.updateConfig(msg)
 
