f62e318267cd645de3ab6deb09098574cdd07f9e
JIngyuan <Andydiantu233@outlook.com>
2024-02-04 23:33:04 +0000

    added file check

--- visionPython/readenv.py
+++ visionPython/readenv.py
@@ -2,6 +2,10 @@ import os
 from dotenv import load_dotenv
 
 env_file = '.env.wakeup'
+file_dir = os.path.dirname(os.path.abspath(__file__))
+print(file_dir)
+if not os.path.exists(file_dir + "/" + env_file):
+    raise Exception("No env file, please use GUI to generate one")
 
 
 load_dotenv(env_file)
