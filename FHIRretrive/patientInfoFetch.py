b49dcf5579cfad80b9a3bb51d767e941125623ab
JIngyuan <Andydiantu233@outlook.com>
2024-02-12 16:29:04 +0000

    added server retrieve name by gosh id

--- FHIRretrive/patientInfoFetch.py
+++ FHIRretrive/patientInfoFetch.py
@@ -1,32 +1,30 @@
 import requests
 import json
 
-class PatientInfoFetch:
-    def __init__(self, serverURL):
-        self.serverURL = serverURL
 
 
-    def nameFetch(self, ID : str):
-        url = self.serverURL + ID
-        response = requests.get(url)
 
-        if response.status_code == 200:
-            patient = json.loads(response.text)
-            print(patient['name'][0]['family'])
-            print(patient['name'][0]['given'])
+def nameFetch(serverURL, ID : str):
+    url = serverURL + ID
+    response = requests.get(url)
 
-            familyName = patient['name'][0]['family']
-            givenName = ' '.join([gName + " " for gName in patient['name'][0]['given']])
+    if response.status_code == 200:
+        patient = json.loads(response.text)
+        print(patient['name'][0]['family'])
+        print(patient['name'][0]['given'])
 
-            return givenName + familyName
-        else:
-            print(f"Error fetching data: {response.status_code}")
+        familyName = patient['name'][0]['family']
+        givenName = ' '.join([gName + " " for gName in patient['name'][0]['given']])
+
+        return givenName.strip(), familyName.strip()
+    else:
+        print(f"Error fetching data: {response.status_code}")
 
 
 
 if __name__ == "__main__":
-    infoFetch = PatientInfoFetch('https://hapi.fhir.org/baseR4/Patient/')
-    print(infoFetch.nameFetch('593047'))
+    givenName,  familyName= nameFetch('https://hapi.fhir.org/baseR4/Patient/', '593047')
+    print(givenName + " " + familyName)
 
 
 
--- wakeup-server/blueprints/users/users.py
+++ wakeup-server/blueprints/users/users.py
@@ -1,25 +1,22 @@
 from flask import Blueprint, request, jsonify
 from models.users import User
 from sqlalchemy.exc import IntegrityError
+from FHIRretrive.patientInfoFetch import nameFetch
 
 users_blueprint = Blueprint('users', __name__)
 
 @users_blueprint.route('/users', methods=['POST'])
-def create_user():
+def create_user(serverURL):
   data = request.get_json()
   
-  first_name = data.get('first_name')
-  last_name = data.get('last_name')
   gosh_id = data.get('gosh_id')
 
-  if first_name is None:
-    return "No user first_name provided", 400
   if gosh_id is None:
     return "No GOSH ID provided", 400
-  if last_name is None:
-    return "No user last_name provided", 400
   
   try:
+    first_name, last_name= nameFetch(serverURL, gosh_id)
+
     user = User(first_name, last_name, gosh_id)
     user.create()
     return user.json(), 200
