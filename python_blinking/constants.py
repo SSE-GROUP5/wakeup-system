a4490e1cc38c4d507a24fbeb5f16152b78320114
David Frischer <david.frischer@frischer.ch>
2024-02-22 17:07:35 +0000

    [blinking] Add wakeup and zmq connection

--- python_blinking/constants.py
+++ python_blinking/constants.py
@@ -1,47 +1,75 @@
 import json
 import os
 from dotenv import load_dotenv
-
+import math
 
 class Constants:
     def __init__(self, envFilename):
         self.file_name = envFilename
+        self.env_vars = {
+            "CLOSED_EYES_FRAME": 10,
+            "BLINKING_RATIO": 4.5,
+            "TIMEOUT_SEC": 5,
+            "WAKEUP_SERVER_URL": None,
+            "ID": None,
+            "ZMQ_SERVER": None
+        }
+        
         file_dir = os.path.dirname(os.path.abspath(__file__))
         if not os.path.exists(file_dir + "/" + self.file_name ):
-            raise Exception("No env file, please use GUI to generate one")
+            raise Exception("No .env.vision file, please generate one")
         
         self.updateConfig()
 
-        if self.CLOSED_EYES_FRAME is None:
-            raise Exception("CLOSED_EYES_FRAME not found in .env")
-        if self.BLINKING_RATIO is None:
-            raise Exception("BLINKING_RATIO not found in .env")
-        if self.TIMEOUT_SEC is None:
-            raise Exception("TIMEOUT_SEC not found in .env")
-        if self.WAKEUP_SERVER_URL is None:
+        if self.env_vars["WAKEUP_SERVER_URL"] is None:
             raise Exception("WAKEUP_SERVER_URL not found in .env")
-        if self.DEVICE_ID is None:
-            raise Exception("DEVICE_ID not found in .env")
+        if self.env_vars["ID"] is None:
+            raise Exception("ID not found in .env.vision")
+        if self.env_vars["ZMQ_SERVER"] is None:
+            raise Exception("ZMQ_SERVER not found in .env.vision")
 
     def updateConfig(self):
         load_dotenv(self.file_name)
+        with open(self.file_name, "r") as file:
+            for line in file:
+                key, value = line.split("=")
+                self.env_vars[key] = value.strip()
+                
+                
+        
+    def get_current_env_vars(self):
+        return self.env_vars
 
-        self.CLOSED_EYES_FRAME = int(os.getenv('CLOSED_EYES_FRAME'))
-        self.BLINKING_RATIO = float(os.getenv('BLINKING_RATIO'))
-        self.TIMEOUT_SEC = int(os.getenv('TIMEOUT_SEC'))
-        self.WAKEUP_SERVER_URL = os.getenv('WAKEUP_SERVER_URL')
-        self.DEVICE_ID = os.getenv('DEVICE_ID')
 
-    def updateEnvFile(self, jsonStr):
-        jsonData = json.load(jsonStr)
-        with open(self.filename, "w") as file:
-            file.write(f"DEVICE_ID={self.DEVICE_ID}")
-            file.write(f"WAKEUP_SERVER_URL={self.WAKEUP_SERVER_URL}")
-            for key in jsonData:
-                file.write(f"{key}={jsonData[key]}")
+    def updateEnvFile(self, new_env_vars):
+        current_env_vars = self.get_current_env_vars()
+            
+        with open(self.file_name, "w") as file:
+            for key, value in new_env_vars.items():
+                file.write(f"{key}={value}\n")
+            for key, value in current_env_vars.items():
+                if key not in new_env_vars:
+                    file.write(f"{key}={value}\n")
 
         # Load the new env file
         self.updateConfig()
+
+
+
+# face bounder indices 
+FACE_OVAL=[ 10, 338, 297, 332, 284, 251, 389, 356, 454, 323, 361, 288, 397, 365, 379, 378, 400, 377, 152, 148, 176, 149, 150, 136, 172, 58, 132, 93, 234, 127, 162, 21, 54, 103,67, 109]
+
+# lips indices for Landmarks
+LIPS=[ 61, 146, 91, 181, 84, 17, 314, 405, 321, 375,291, 308, 324, 318, 402, 317, 14, 87, 178, 88, 95,185, 40, 39, 37,0 ,267 ,269 ,270 ,409, 415, 310, 311, 312, 13, 82, 81, 42, 183, 78 ]
+LOWER_LIPS =[61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291, 308, 324, 318, 402, 317, 14, 87, 178, 88, 95]
+UPPER_LIPS=[ 185, 40, 39, 37,0 ,267 ,269 ,270 ,409, 415, 310, 311, 312, 13, 82, 81, 42, 183, 78] 
+# Left eyes indices 
+LEFT_EYE =[ 362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385,384, 398 ]
+LEFT_EYEBROW =[ 336, 296, 334, 293, 300, 276, 283, 282, 295, 285 ]
+
+# right eyes indices
+RIGHT_EYE=[ 33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161 , 246 ]  
+RIGHT_EYEBROW=[ 70, 63, 105, 66, 107, 55, 65, 52, 53, 46 ]
 
 
 
--- python_blinking/utils.py
+++ python_blinking/utils.py
@@ -7,6 +7,7 @@ Youtube Channel: https://www.youtube.com/c/aiphile
 
 import cv2 as cv 
 import numpy as np
+import math
 
 # colors 
 # values =(blue, green, red) opencv accepts BGR values not RGB
@@ -154,6 +155,60 @@ def rectTrans(img, pt1, pt2, color, thickness, opacity):
     img = new_img
 
     return img
+
+# landmark detection function 
+def landmarksDetection(img, results, draw=False):
+    img_height, img_width= img.shape[:2]
+    mesh_coord = [(int(point.x * img_width), int(point.y * img_height)) for point in results.multi_face_landmarks[0].landmark]
+    if draw :
+        [cv.circle(img, p, 2, (0,255,0), -1) for p in mesh_coord]
+
+    # returning the list of tuples for each landmarks 
+    return mesh_coord
+
+# Euclaidean distance 
+def euclaideanDistance(point, point1):
+    x, y = point
+    x1, y1 = point1
+    distance = math.sqrt((x1 - x)**2 + (y1 - y)**2)
+    return distance
+  
+# Blinking Ratio
+def blinkRatio(img, landmarks, right_indices, left_indices):
+    # Right eyes 
+    # horizontal line 
+    rh_right = landmarks[right_indices[0]]
+    rh_left = landmarks[right_indices[8]]
+    # vertical line 
+    rv_top = landmarks[right_indices[12]]
+    rv_bottom = landmarks[right_indices[4]]
+    # draw lines on right eyes 
+    # cv.line(img, rh_right, rh_left, utils.GREEN, 2)
+    # cv.line(img, rv_top, rv_bottom, utils.WHITE, 2)
+
+    # LEFT_EYE 
+    # horizontal line 
+    lh_right = landmarks[left_indices[0]]
+    lh_left = landmarks[left_indices[8]]
+
+    # vertical line 
+    lv_top = landmarks[left_indices[12]]
+    lv_bottom = landmarks[left_indices[4]]
+
+    rhDistance = euclaideanDistance(rh_right, rh_left)
+    rvDistance = euclaideanDistance(rv_top, rv_bottom)
+
+    lvDistance = euclaideanDistance(lv_top, lv_bottom)
+    lhDistance = euclaideanDistance(lh_right, lh_left)
+
+    reRatio = rhDistance/rvDistance
+    leRatio = lhDistance/lvDistance
+
+    ratio = (reRatio+leRatio)/2
+    return ratio 
+
+
+
 
 def main():
     cap = cv.VideoCapture('Girl.mp4')
