f067a761f37008ce1ab06368c16325aa0353024f
David Frischer <david.frischer@frischer.ch>
2024-02-26 15:54:24 +0000

    [exe] Make blinking and fall detection for exe file

--- fall_detection/run_build.sh
+++ fall_detection/run_build.sh
@@ -2,15 +2,16 @@
 set -e
 DIRECTORY=$(dirname $0)
 
+
 # Check if the venv exists
 if [ ! -d "$DIRECTORY/../venv/lib/python3.10" ]; then
-  echo "Please create a virtual environment first with python3.10"
+  echo "Please create a virtual environment with folder name venv and with python3.10"
   echo "Eg. python3.10 -m venv ../venv"
   exit 1
 fi
 
 pip3 install zmq
-pip3 install mediapipe
+pip3 install ultralytics
 pip3 install simpleaudio
 
 pyinstaller \
@@ -18,5 +19,5 @@ pyinstaller \
   --add-data "$DIRECTORY/../beepy:beepy" \
   --hidden-import=simpleaudio \
   --add-data "$DIRECTORY/../venv/lib/python3.10/site-packages/zmq:zmq" \
-  --add-data "$DIRECTORY/../venv/lib/python3.10/site-packages/mediapipe:mediapipe" \
-  --onefile "$DIRECTORY/blink_detect.py"          
+  --add-data "$DIRECTORY/../venv/lib/python3.10/site-packages/ultralytics:ultralytics" \
+  --onefile "$DIRECTORY/yolov8_fall_upper_detection.py"
