bc741634c2aaee9e59dd41a5c2c64c96cd5a11d4
EthanSun11 <79974956+EthanSun11@users.noreply.github.com>
2024-02-28 00:10:15 +0000

    [testing] More tests with pytest
    * tests with exceptions
    
    * Delete wakeup-server/tests/test_interactive_devices_exception.py
    
    * tests update with no self-call and 6 new tests passed
    
    * small change
    
    ---------
    
    Co-authored-by: EthanSun11 <ethan078@gmail.com>

--- wakeup_server/tests/test_signals.py
+++ wakeup_server/tests/test_signals.py
@@ -1,10 +1,6 @@
 import pytest
 
 
-def test_get_signal_for_user(client):
-    pass
-
-
 def test_receive_signal(client):
     pass
     
@@ -12,17 +8,22 @@ def test_receive_signal(client):
 def test_set_signal(client):
     trigger = client.post("/triggers", json={"name": "andy_vision", "type": "SOUND"})
     trigger_id = trigger.get_json()["id"]
+    trigger_name = trigger.get_json()["name"]
     target = client.post("/target_devices", json={"matter_id": "switch.smart_plug_mini2", "name": "Fake Matter Device", "type": "switch"})
     target_id = target.get_json()["matter_id"]
+    user = client.post("/users", json = {"first_name": "test", "last_name": "user", "gosh_id": "001"})
+    user_id = user.get_json()["id"]
+
     
     response = client.post("/signals/set", json = {
         "trigger_id": trigger_id,
+        "trigger_name": trigger_name,
         "trigger_action": "taps",
         "trigger_num_actions": 3, 
         "target_device_id": target_id,
-        "target_action": "turn_off"
+        "target_action": "turn_off",
+        "user_id": user_id
     })
-   
     body = response.get_json()
     assert response.status_code == 201
     assert "Signal set" == body["message"]
@@ -35,9 +36,52 @@ def test_set_signal(client):
     
     
 def test_get_signal(client):
-    test_set_signal(client)
+    trigger = client.post("/triggers", json={"name": "andy_vision", "type": "SOUND"})
+    trigger_id = trigger.get_json()["id"]
+    trigger_name = trigger.get_json()["name"]
+    target = client.post("/target_devices", json={"matter_id": "switch.smart_plug_mini2", "name": "Fake Matter Device", "type": "switch"})
+    target_id = target.get_json()["matter_id"]
+    user = client.post("/users", json = {"first_name": "test", "last_name": "user", "gosh_id": "001"})
+    user_id = user.get_json()["id"]
+
+    client.post("/signals/set", json = {
+        "trigger_id": trigger_id,
+        "trigger_name": trigger_name,
+        "trigger_action": "taps",
+        "trigger_num_actions": 3, 
+        "target_device_id": target_id,
+        "target_action": "turn_off",
+        "user_id": user_id
+    })
+
     response = client.get("/signals")
     signal_list = response.get_json()['signals']
     assert response.status_code == 200 and len(signal_list) == 1
 
     return signal_list
+
+
+def test_get_signal_for_user(client):
+    trigger = client.post("/triggers", json={"name": "andy_vision", "type": "SOUND"})
+    trigger_id = trigger.get_json()["id"]
+    trigger_name = trigger.get_json()["name"]
+    target = client.post("/target_devices", json={"matter_id": "switch.smart_plug_mini2", "name": "Fake Matter Device", "type": "switch"})
+    target_id = target.get_json()["matter_id"]
+    user = client.post("/users", json = {"first_name": "test", "last_name": "user", "gosh_id": "001"})
+    user_id = user.get_json()["id"]
+    
+    client.post("/signals/set", json = {
+        "trigger_id": trigger_id,
+        "trigger_name": trigger_name,
+        "trigger_action": "taps",
+        "trigger_num_actions": 3, 
+        "target_device_id": target_id,
+        "target_action": "turn_off",
+        "user_id": user_id
+    })
+
+    response = client.get("/signals/users/"+user_id)
+    signal_list = response.get_json()['signals']
+    assert response.status_code == 200 and len(signal_list) == 1
+
+
--- wakeup_server/tests/test_users.py
+++ wakeup_server/tests/test_users.py
@@ -10,11 +10,9 @@ def test_create_user(client):
     gosh_id = user['gosh_id']
     assert response.status_code == 200 and len(id) == 36 and first_name == "test" and last_name == "user" and gosh_id == "001"
 
-    return user
-
 
 def test_get_user_by_id(client):
-    obj = test_create_user(client)
+    obj = client.post("/users", json = {"first_name": "test", "last_name": "user", "gosh_id": "001"}).get_json()
     response = client.get("/users/"+obj['id'])
     user = response.get_json()
     assert response.status_code == 200 and obj['first_name'] == user['first_name'] and obj['last_name'] == user['last_name'] and obj['gosh_id'] == user['gosh_id']
@@ -29,44 +27,20 @@ def test_get_user(client):
     assert response.status_code == 200 and obj1['id'] in id_list and obj2['id'] in id_list
 
 
-@pytest.mark.skip(reason="Not implemented")
-def test_too_long_first_name(client):
-    response = client.post("/users", json = {"first_name": "a"*1000, "last_name": "user", "gosh_id": "001"})
-    user = response.get_json()
-    with pytest.raises(TypeError) as e:
-        first_name = user['first_name']
-    msg = e.value.args[0]
-    assert 'NoneType' in msg
-
-
-@pytest.mark.skip(reason="Not implemented")
-def test_too_long_last_name(client):
-    response = client.post("/users", json = {"first_name": "test", "last_name": "a"*1000, "gosh_id": "001"})
-    user = response.get_json()
-    with pytest.raises(TypeError) as e:
-        last_name = user['last_name']
-    msg = e.value.args[0]
-    assert 'NoneType' in msg
-
-
-@pytest.mark.skip(reason="Not implemented")
 def test_empty_first_name(client):
     response = client.post("/users", json = {"first_name": "", "last_name": "user", "gosh_id": "001"})
-    user = response.get_json()
-    with pytest.raises(TypeError) as e:
-        first_name = user['first_name']
-    msg = e.value.args[0]
-    assert 'NoneType' in msg
+    assert b"Unknown error" in response.data
+
+
+def test_empty_last_name(client):
+    response = client.post("/users", json = {"first_name": "test", "last_name": "", "gosh_id": "001"})
+    assert b"Unknown error" in response.data
 
 
 @pytest.mark.skip(reason="Not implemented")
-def test_empty_last_name(client):
-    response = client.post("/users", json = {"first_name": "test", "last_name": "", "gosh_id": "001"})
-    user = response.get_json()
-    with pytest.raises(TypeError) as e:
-        last_name = user['last_name']
-    msg = e.value.args[0]
-    assert 'NoneType' in msg
+def test_empty_gosh_id(client):
+    response = client.post("/users", json = {"first_name": "test", "last_name": "user", "gosh_id": ""})
+    assert b"Unknown error" in response.data
 
 
 @pytest.mark.skip(reason="Not implemented")
@@ -83,12 +57,12 @@ def test_duplicate_gosh_id(client):
 
 
 def test_delete_user(client):
-    obj = test_create_user(client)
+    obj = client.post("/users", json = {"first_name": "test", "last_name": "user", "gosh_id": "001"}).get_json()
     response = client.delete("/users/"+obj['id'])
     assert b"User deleted successfully" in response.data
 
 
 def test_update_user(client):
-    obj = test_create_user(client)
+    obj = client.post("/users", json = {"first_name": "test", "last_name": "user", "gosh_id": "001"}).get_json()
     response = client.put("/users/"+obj['id'], json = {"first_name": "update", "last_name": "modified", "gosh_id": "XXX"})
     assert b"User updated successfully" in response.data
