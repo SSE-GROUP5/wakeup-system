33cfb7d258dc4aaba632b242a944a67514a2ea97
David <david.frischer@unifr.ch>
2024-02-28 17:31:19 +0000

    [exe] Make it windows compatible

--- fall_detection/run_build.sh
+++ fall_detection/run_build.sh
@@ -3,21 +3,33 @@ set -e
 DIRECTORY=$(dirname $0)
 
 
-# Check if the venv exists
-if [ ! -d "$DIRECTORY/../venv/lib/python3.10" ]; then
-  echo "Please create a virtual environment with folder name venv and with python3.10"
+# Check if it is python3.10 
+if [[ $(python3 --version) != *"3.10"* ]]; then
+  echo "Please create a virtual environment first with python3.10"
   echo "Eg. python3.10 -m venv ../venv"
+  echo "Then activate the virtual environment"
   exit 1
 fi
 
-pip3 install zmq
-pip3 install ultralytics
-pip3 install simpleaudio
+pip3 install zmq requests python-dotenv ultralytics simpleaudio
+
+# check if windows 
+IS_WINDOWS=false
+if [[ "$OSTYPE" == "msys" ]]; then
+  IS_WINDOWS=true
+fi
+
+ADD_DATA=""
+if [ "$IS_WINDOWS" = true ]; then
+  ADD_DATA="--add-data $DIRECTORY/../venv/Lib/site-packages/ultralytics:ultralytics"
+else
+  ADD_DATA="--add-data $DIRECTORY/../venv/lib/python3.10/site-packages/ultralytics:ultralytics"
+fi
 
 pyinstaller \
   --add-data "$DIRECTORY/../zeromq:zeromq" \
   --add-data "$DIRECTORY/../beepy:beepy" \
+    $ADD_DATA \
   --hidden-import=simpleaudio \
-  --add-data "$DIRECTORY/../venv/lib/python3.10/site-packages/zmq:zmq" \
-  --add-data "$DIRECTORY/../venv/lib/python3.10/site-packages/ultralytics:ultralytics" \
+  --hidden-import=zmq \
   --onefile "$DIRECTORY/yolov8_fall_upper_detection.py"
--- python_blinking/run_build.sh
+++ python_blinking/run_build.sh
@@ -2,21 +2,34 @@
 set -e
 DIRECTORY=$(dirname $0)
 
-# Check if the venv exists
-if [ ! -d "$DIRECTORY/../venv/lib/python3.10" ]; then
+# Check if it is python3.10 
+if [[ $(python3 --version) != *"3.10"* ]]; then
   echo "Please create a virtual environment first with python3.10"
   echo "Eg. python3.10 -m venv ../venv"
+  echo "Then activate the virtual environment"
   exit 1
 fi
 
-pip3 install zmq
-pip3 install mediapipe
-pip3 install simpleaudio
+pip3 install zmq requests python-dotenv mediapipe simpleaudio
+
+# check if windows 
+IS_WINDOWS=false
+if [[ "$OSTYPE" == "msys" ]]; then
+  IS_WINDOWS=true
+fi
+
+ADD_DATA_MEDIAPIPE=""
+if [ "$IS_WINDOWS" = true ]; then
+  ADD_DATA_MEDIAPIPE="--add-data $DIRECTORY/../venv/Lib/site-packages/mediapipe:mediapipe"
+else
+  ADD_DATA_MEDIAPIPE="--add-data $DIRECTORY/../venv/lib/python3.10/site-packages/mediapipe:mediapipe \
+  --add-data $DIRECTORY/../venv/lib/python3.10/site-packages/mediapipe/modules/face_landmark:mediapipe/modules/face_landmark"
+fi
 
 pyinstaller \
   --add-data "$DIRECTORY/../zeromq:zeromq" \
   --add-data "$DIRECTORY/../beepy:beepy" \
   --hidden-import=simpleaudio \
-  --add-data "$DIRECTORY/../venv/lib/python3.10/site-packages/zmq:zmq" \
-  --add-data "$DIRECTORY/../venv/lib/python3.10/site-packages/mediapipe:mediapipe" \
+  --hidden-import=zmq \
+   $ADD_DATA_MEDIAPIPE \
   --onefile "$DIRECTORY/blink_detect.py"          
