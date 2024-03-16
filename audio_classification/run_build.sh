8d05924107ce386cce5d74fdefeaca4415a386c4
dias2406 <ucabdj5@ucl.ac.uk>
2024-03-16 20:47:20 +0000

    [sound] added trigger configuration and readme for whisper and classification

--- audio_classification/run_build.sh
+++ audio_classification/run_build.sh
@@ -2,9 +2,33 @@
 set -e
 DIRECTORY=$(dirname $0)
 
+
+# Check if it is python3.10 
+if [[ $(python3 --version) != *"3.10"* ]]; then
+  echo "Please create a virtual environment first with python3.10"
+  echo "Eg. python3.10 -m venv ../sound_venv"
+  echo "Then activate the virtual environment"
+  exit 1
+fi
+
+pip3 install zmq requests python-dotenv  
+
+# check if windows 
+IS_WINDOWS=false
+if [[ "$OSTYPE" == "msys" ]]; then
+  IS_WINDOWS=true
+fi
+
+ADD_DATA=""
+if [ "$IS_WINDOWS" = true ]; then
+  ADD_DATA="--add-data $DIRECTORY/../sound_venv/Lib/site-packages/mediapipe:mediapipe"
+else
+  ADD_DATA="--add-data $DIRECTORY/../sound_venv/lib/python3.10/site-packages/mediapipe:mediapipe"
+fi
+
 pyinstaller \
   --add-data "$DIRECTORY/yamnet.tflite:." \
-  --hidden-import=tkinter \
-  --hidden-import=requests \
-  --add-data "$DIRECTORY/../triggers_gui_config:triggers_gui_config" \
+  --add-data "$DIRECTORY/../zeromq:zeromq" \
+    $ADD_DATA \
+  --hidden-import=zmq \
   --onefile "$DIRECTORY/classify.py"   
