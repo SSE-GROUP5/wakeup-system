88c88bd88338894cf2eb061fb3c9455bc1347bd6
EthanSun11 <79974956+EthanSun11@users.noreply.github.com>
2024-03-18 10:05:25 +0000

    Update test_signals.py

--- wakeup_server/tests/test_signals.py
+++ wakeup_server/tests/test_signals.py
@@ -9,24 +9,17 @@ def test_set_signal(client):
     trigger = client.post("/triggers", json={"name": "andy_vision", "type": "vision_blink"})
     trigger_id = trigger.get_json()["id"]
     trigger_name = trigger.get_json()["name"]
-    trigger_name = trigger.get_json()["name"]
     target = client.post("/target_devices", json={"matter_id": "switch.smart_plug_mini2", "name": "Fake Matter Device", "type": "switch"})
     target_id = target.get_json()["matter_id"]
     user = client.post("/users", json = {"first_name": "test", "last_name": "user", "gosh_id": "001"})
     user_id = user.get_json()["id"]
 
-    user = client.post("/users", json = {"first_name": "test", "last_name": "user", "gosh_id": "001"})
-    user_id = user.get_json()["id"]
-
-    
     response = client.post("/signals/set", json = {
         "trigger_id": trigger_id,
         "trigger_name": trigger_name,
         "trigger_action": "vision_blink",
         "trigger_num_actions": 3, 
         "target_device_id": target_id,
-        "target_action": "turn_off",
-        "user_id": user_id
         "target_action": "turn_off",
         "user_id": user_id
     })
