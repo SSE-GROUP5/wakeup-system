dcdaf7039d456a2b6b4a82d9f3e24dee724f87a6
dias2406 <ucabdj5@ucl.ac.uk>
2024-03-18 13:41:11 +0000

    [setup] added pyinstaller for classification
    [setup] pyinstaller for openvino whisper
    
    update
    
    bug fixed
    
     bug fix
    
    [Morse_vision] fixed executable file name for morse vision
    
    [Morse exe fix]fix undelete commented code

--- morse_vision/run_build.sh
+++ morse_vision/run_build.sh
@@ -2,15 +2,6 @@
 set -e
 DIRECTORY=$(dirname $0)
 
-# Check if it is python3.10 
-if [[ $(python3 --version) != *"3.10"* ]]; then
-  echo "Please create a virtual environment first with python3.10"
-  echo "Eg. python3.10 -m venv venv"
-  echo "Then activate the virtual environment"
-  exit 1
-fi
-
-
 # check if windows 
 IS_WINDOWS=false
 if [[ "$OSTYPE" == "msys" ]]; then
@@ -23,6 +14,8 @@ if [ "$IS_WINDOWS" = true ]; then
 else
   ADD_DATA="--add-data $DIRECTORY/venv/lib/python3.10/site-packages/mediapipe:mediapipe"
 fi
+
+pip3 install zmq requests python-dotenv ultralytics simpleaudio
 
 
 pyinstaller \
